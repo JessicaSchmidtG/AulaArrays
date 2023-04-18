f1dfe5357ef6fa8a74f63d9aff474abe33620138
Jessica Schmidt <jessyka_sl@hotmail.com>
2023-04-18 19:58:37 -0300

    Aula Arrays

---
+++ index.js
@@ -0,0 +1,19 @@
+//Para este exercicio, comece criando
+//um array com os valores: 1,2,3,4,5,6
+
+const valores =[1,2,3,4,5,6];
+
+//Determine o tamanho do array
+//Adicione o número 7
+valores.push(7);
+//alert(valores.length);
+
+//Remova os números 4 e 5
+valores.splice(3,2);
+//alert(valores.length);
+
+//Determine o novo tamanho do array
+alert(valores.length)
+
+
+
